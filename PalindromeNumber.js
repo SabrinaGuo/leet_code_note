5691a72fadbfa698444608123ed915f8e06f4186
Sabrina <hsif0111@gmail.com>
2020-04-24 11:31:39 +0800

    feat 第九題自己的答案

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -5,17 +5,13 @@
 var x = 121;
 var isPalindrome = function(x) {
     var solution = x.toString().split('');
-    var last = (solution.length) - 1;
+    var last = (solution.length) -1 ;
     var checkTime = Math.floor((solution.length) / 2);
-    var checkTime = Math.floor((solution.length) / 2);
-    for( var i = 0; i<= checkTime ; i++){
-            if(solution[i] !== solution[last - 1 - i]){
-                console.log("F")
-                return false;
-            }
+for( var i = 0; i<= checkTime ; i++){
+        if(solution[i] !== solution[last - i]){
+            return false;
         }
-        console.log("T")
-        return true;
+    }
+   return true;
 };
-
 isPalindrome(x);
